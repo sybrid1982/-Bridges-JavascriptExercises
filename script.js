d9c7f56a5155bfa6c76b5f563b0da4af475a2db0
Sydney Bridges <sybrid@gmail.com>
2018-07-07 13:52:33 -0400

    Added javascript output to HTML element on index page along with console output

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function numberGame() {
   var number = prompt('Enter a number!');
-  console.log(numberAdder(number));
+  var newNumber = numberAdder(number);
+  console.log(newNumber);
+  var outputDOM = document.querySelector('#output > p');
+  outputDOM.textContent = newNumber;
+  outputDOM.style.display = 'block';
 }
 
 function numberAdder(number) {
@@ -15,6 +19,10 @@ function numberAdder(number) {
 function stringBuilder() {
   var keepGoing;
   var finalString = '';
+
+  var outputDOM = document.querySelector('#output > p');
+  outputDOM.style.display = 'none';
+
   keepGoing = prompt('Do you want to play?');
 
   if(keepGoing==='yes'){
@@ -29,15 +37,21 @@ function stringBuilder() {
     }
     while(keepGoing==='yes')
     console.log(finalString);
+    outputDOM.textContent = finalString;
+    outputDOM.style.display = 'block';
   }
 }
 
 function nameExciter() {
+  var outputDOM = document.querySelector('#output > p');
   var name = prompt('What is your name?');
   var play = prompt('Would you like to print your name?');
   var exclamations = '';
   while(play==='yes') {
-    console.log('Hello.  My name is ' + name + exclamations);
+    var output = 'Hello.  My name is ' + name + exclamations;
+    console.log(output)
+    outputDOM.style.display = 'block';
+    outputDOM.textContent = output;
     exclamations += '!';
     play = prompt('Would you like to play again?');
   }
@@ -46,6 +60,8 @@ function nameExciter() {
 function mealPrompter() {
   var timeOfDay = prompt('What time of day is it?');
   var mealType, meal;
+  var outputDOM = document.querySelector('#output > p');
+
   switch(timeOfDay){
     case 'morning':
       mealType = 'breakfast';
@@ -64,9 +80,14 @@ function mealPrompter() {
       meal = '';
       break;
   }
+  var output;
   if(mealType !== ''){
-    console.log('Since it is ' + timeOfDay + ' you should be eating ' + mealType + '. We suggest ' + meal + '.');
+    output = ('Since it is ' + timeOfDay + ' you should be eating ' + mealType + '. We suggest ' + meal + '.');
   } else {
-    console.log("We don't know what time of day " + timeOfDay + ' is.');
+    output = ("We don't know what time of day " + timeOfDay + ' is.');
   }
+
+  console.log(output);
+  outputDOM.style.display = 'block';
+  outputDOM.textContent = output;
 }
